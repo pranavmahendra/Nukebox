66dec9d1dbea5bb14c64845a996d81b6c58abd1d
Pranav Mahendra <>
2020-07-25 16:16:43 +0530

    1) Made all boxes as grey. 2) Score won't update once the line is completed.

---
+++ Assets/Scripts/Raycast.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+
+    Vector3 mousePos;
+    Camera mainCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //mousePos = new Vector3(mainCam.WorldToScreenPoint(Input.GetMouseButtonDown(0));
+        }
+    }
+}
--- Assets/Scripts/Square_View.cs
+++ Assets/Scripts/Square_View.cs
@@ -16,17 +16,19 @@ public class Square_View : MonoBehaviour
 
     public int ID;
     //public Color TextColor;
-    public Color imageColor;
+    public Color imageColor; //This is what visbile in game.
 
     [HideInInspector] public Button buttonComponent; //Action will take place
     [HideInInspector] public Image imageComponent;
 
     public bool isHead;   //Whether this is head or not.
-    private  bool firstHeadFound = false;
+    private static bool firstHeadFound;
+    //private static bool inProgress = false; //Static variable
 
     //event actions
     public UnityEvent flowCompleted;
     public UnityEvent moveCompleted;
+
 
     [HideInInspector]
     public static List<Square_View> matchFoundlist = new List<Square_View>(); //This is where the matching box colors will be stored.
@@ -35,7 +37,7 @@ public class Square_View : MonoBehaviour
 
     private void Awake()
     {
-        PlayerPrefs.DeleteKey("ID"); //Will clear if any ID is available.
+        PlayerPrefs.DeleteAll(); //Will clear if any ID is available.
     }
 
     void Start()
@@ -43,28 +45,43 @@ public class Square_View : MonoBehaviour
         //buttonText = GetComponentInChildren<Text>();
 
         imageComponent = GetComponent<Image>();
-        imageColor = imageComponent.color;
+        //imageColor = imageComponent.color;  //First getting set as per inspector.
 
-        if (isHead != true)
-        {
-            //buttonText.color = Color.white;
-            //buttonText.fontSize = 20;
-            imageComponent.color = Color.gray;
-        }
+        //if (isHead != true)  //For buttons which are not head. Color is set to grey.
+        //{
+        //    //buttonText.color = Color.white;
+        //    //buttonText.fontSize = 20;
 
+        //}
+        imageComponent.color = Color.gray;  //Gray color being assigned.
 
         buttonComponent = GetComponent<Button>();
         buttonComponent.onClick.AddListener(GetSavedIDValue);
+        buttonComponent.onClick.AddListener(ChnageColorHead);
     }
 
+    private void Update()
+    {
+        Debug.Log("Status of first head is:" + firstHeadFound);
+    }
 
+    //Changing the color of button when pressed as per the defined image color in inspector.
+    public void ChnageColorHead()
+    {
+        if (isHead)
+        {
+            imageComponent.color = imageColor; //Setting the color saved at line 45.
+        }
+        savingColor(imageColor);  //Saving the values of color saved at line 45.
+    }
 
     public void GetSavedIDValue()
     {
-   
+        //Debug.Log("Status of first head is:" + firstHeadFound);
+
         int x = 0;
         Color setColor;
-        
+
 
         if (PlayerPrefs.HasKey("ID")) //if id is found 2 conditions will be checked.
         {
@@ -73,58 +90,67 @@ public class Square_View : MonoBehaviour
             //Setting color values as per player prefs.
             setColor = new Color(PlayerPrefs.GetFloat("rValue", 1F), PlayerPrefs.GetFloat("gValue", 1F), PlayerPrefs.GetFloat("bValue", 1F), PlayerPrefs.GetFloat("aValue", 1F));
 
-            Debug.Log("The saved id is: " + x);
+            //Debug.Log("The saved id is: " + x);
             if (x == ID) //Case1 - If ID of next box Matches
             {
-                if (isHead != true)
+                if (isHead != true) //Normal boxes
                 {
-                    Debug.Log("The match has been found for Color: " + imageColor.ToString());
+                    //Debug.Log("The match has been found for Color: " + imageColor.ToString());
 
+                    //inProgress = true;
                     imageColor = setColor;
                     imageComponent.color = imageColor; //Applying the saved value in playerprefs to the image color.
 
-                    //buttonText.color = TextColor;
-
+                    
                     matchFoundlist.Add(this);
-                    Debug.Log("Total count of the list is: " + matchFoundlist.Count);
+                    //Debug.Log("Total count of the list is: " + matchFoundlist.Count);
                 }
-                else if (isHead && !firstHeadFound) //Other head will always have this value as false by default.
+                else if (isHead && firstHeadFound) //This means line is completed.
                 {
+                    firstHeadFound = false;
                     Debug.Log("This line is completed.");
 
+                    //inProgress = false;
+
                     flowCompleted.Invoke();  //Event invoked means line has been completed.
-                      
                     moveCompleted.Invoke();
 
                     //PlayerPrefs.DeleteAll();
                     PlayerPrefs.DeleteKey("ID");
+
                     //This will invoke a method for achievement system.
-                    matchFoundlist.Clear();
-                    Debug.Log("Setting firsthead to: " + firstHeadFound);
+                    DisableList();
+                    //Debug.Log("Setting firsthead to: " + firstHeadFound);
                 }
+
             }
             else   //Case2 - If ID of next box does not match.
             {
                 if (isHead)
                 {
+                    firstHeadFound = true;    //Both selected are heads so no need to chnge the status of head.
                     moveCompleted.Invoke();
-
-                    PlayerPrefs.SetInt("ID", ID);  
+                    //inProgress = false;
+                    PlayerPrefs.SetInt("ID", ID);
                     savingColor(imageComponent.color);
 
-                }
-                else
-                {
-
-                    PlayerPrefs.DeleteKey("ID");
-
-                    PlayerPrefs.SetString("Failed","true");
-                 
+                    //Reseting previous list.
                     resetColors();
                     matchFoundlist.Clear();
 
-                    Debug.Log("Deleting Key");
-                    Debug.Log("Clearing list. Count is: " + matchFoundlist.Count);
+                }
+                else  //Clear everything
+                {
+                    firstHeadFound = false;
+                    PlayerPrefs.DeleteKey("ID");
+
+                    //PlayerPrefs.SetString("Failed","true");
+
+                    resetColors();
+                    matchFoundlist.Clear();
+
+                    //Debug Log("Deleting Key");
+                    //Debug.Log("Clearing list. Count is: " + matchFoundlist.Count);
                 }
 
 
@@ -133,12 +159,12 @@ public class Square_View : MonoBehaviour
         }
         else   //if ID is not found
         {
-            Debug.Log("This should run!!");
-            if (isHead && !firstHeadFound)
+            //Debug.Log("This should run!!");
+            if (isHead && !firstHeadFound)  //Will run when game first starts.
             {
                 firstHeadFound = true; //bool is getting true.
+                matchFoundlist.Add(this);
 
-                Debug.Log("Status if first head is:" + firstHeadFound);
 
                 PlayerPrefs.SetInt("ID", ID); //if no id is available then this will be called.
 
@@ -146,20 +172,22 @@ public class Square_View : MonoBehaviour
 
                 savingColor(imageComponent.color);   //Saving color from the image of the button.
 
-                Debug.Log("Saving ID" + ID + ". Color saved is: " + imageColor.ToString());
+                Debug.Log("Saving ID: " + ID + ". Color saved is: " + imageColor.ToString());
             }
-            else if (isHead && firstHeadFound)
-            {
-            
-               if (PlayerPrefs.HasKey("Failed"))
-                {
-                    PlayerPrefs.SetInt("ID", ID);
-                    savingColor(imageComponent.color);
-                    PlayerPrefs.DeleteKey("Failed");
-                }
-                
-            }
-         
+            //else if (isHead && firstHeadFound)
+            //{
+
+            //   if (PlayerPrefs.HasKey("Failed"))
+            //    {
+            //        firstHeadFound = true;
+
+            //        PlayerPrefs.SetInt("ID", ID);
+            //        savingColor(imageComponent.color);
+            //        PlayerPrefs.DeleteKey("Failed");
+            //    }
+
+            //}
+
         }
     }
 
@@ -173,7 +201,7 @@ public class Square_View : MonoBehaviour
         PlayerPrefs.SetFloat("bValue", colorOfButton.b);
         PlayerPrefs.SetFloat("aValue", colorOfButton.a);
 
-        Debug.Log("Saving color values." + colorOfButton.r + colorOfButton.g + colorOfButton.b);
+        //Debug.Log("Saving color values." + colorOfButton.r + colorOfButton.g + colorOfButton.b);
     }
 
     //Reseting the color data of the images in the list
@@ -185,8 +213,21 @@ public class Square_View : MonoBehaviour
         for (int i = 0; i < matchFoundlist.Count; i++)
         {
             matchFoundlist[i].imageComponent.color = Color.gray;
-            
+
         }
+    }
+
+    //Disable list as soon as move is completed.
+    private void DisableList()
+    {
+        matchFoundlist.Add(this);
+        //Debug.Log("Clearing list. Count is: " + matchFoundlist.Count);
+        for (int i = 0; i < matchFoundlist.Count ; i++)
+        {
+            matchFoundlist[i].imageComponent.raycastTarget = false;
+            //Debug.Log("Disabling");
+        }
+        matchFoundlist.Clear();
     }
 
 }
